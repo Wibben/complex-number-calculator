3a183ee2b42200560b4661adc35f259ba242f4b2
Justin Kreiner <j_kreiner@outlook.com>
2022-01-19 13:20:41 -0500

    Implemented nth root functionality that displays one solution

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { multiply } from "mathjs";
+import { multiply, range } from "mathjs";
 import { functions, specialFunctions } from "./math";
 
 export function snapSelectionToInput(array, selection) {
@@ -79,4 +79,22 @@ export function convertToRadians(angle, mode) {
       break;
   }
   return result;
+}
+
+export function digitToSuperscript(n) {
+  var supers = {"0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴", "5": "⁵", "6": "⁶", "7": "⁷", "8": "⁸", "9": "⁹"};
+  var result = [];
+  for (var i = 0; i < n.length; i++) {
+    result.push(supers[n[i]]);
+  }
+  return result.join("");
+}
+
+export function superscriptToDigit(n) {
+  var supers = {"⁰": "0", "¹": "1", "²": "2", "³": "3", "⁴": "4", "⁵": "5", "⁶": "6", "⁷": "7", "⁸": "8", "⁹": "9"};
+  var result = [];
+  for (var i = 0; i < n.length; i++) {
+    result.push(supers[n[i]]);
+  }
+  return result.join("");
 }
